eb70c7d10ae75873860262860309d9f596936f97
Caitlyn Kidd <caitlyn.kidd92@gmail.com>
2022-03-03 17:30:21 -0700

    .

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const sequelize = require('./config/connection');
 const path = require('path');
-const routes = require('./controllers');
-const exphbs = require('express-handlebars');
+const express = require('express');
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
-require('dotenv').config();
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
+const hbs = exphbs.create({ helpers });
+
 const sess = {
-  secret: 'supersecretsessionsecrettext',
-  cookie: { maxAge: 180000},
+  secret: 'specialsecret',
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -23,8 +23,9 @@ const sess = {
   })
 };
 
-//handlebars initialization
-const hbs = exphbs.create({ helpers });
+app.use(session(sess));
+
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -32,11 +33,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session(sess));
-
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
